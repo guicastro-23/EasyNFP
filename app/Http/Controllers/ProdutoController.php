8b5f2262d7998f70071ffa335a7bb9482d1f0d6d
Guilherme Castro <guilherme.gcastro20@gmail.com>
2025-11-14 01:17:45 -0300

    Inicio de produtos

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
+use Inertia\Response;
+
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
-    //
+    public function index(): Response 
+    {
+        return Inertia::render('Produto/Index', [
+            
+        ]);
+    }
 }
