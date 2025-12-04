683bf54339167d8183c3d6f233029988ee95acac
Guilherme Castro <guilherme.gcastro20@gmail.com>
2025-12-04 13:46:10 -0300

    Inicio Tela de Produtos

--- resources/js/Pages/Produto/Index.jsx
+++ resources/js/Pages/Produto/Index.jsx
@@ -1,8 +1,18 @@
-// import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-// import { Link } from '@inertiajs/react';
+ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+ import { Link } from '@inertiajs/react';
 
-// export default function Index({ produtos }) {
-//     <AuthenticatedLayout>
-//         dddd
-//     </AuthenticatedLayout>
-// }
+ export default function Index({ produtos }) {
+    return(
+        <AuthenticatedLayout>
+            <div className='max-w-5xl mx-auto p-4'>
+                <Link
+                    href="/produto/cadastrar"
+                    className='mb-4 inline-block px-4 py-2 bg-blue-600 text-white rounded'
+                >
+                    Novo Produto
+                </Link>
+            </div>
+        </AuthenticatedLayout>
+    )
+
+}
